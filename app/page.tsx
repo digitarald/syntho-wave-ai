a23c635acd3ce6a675126fc1326a2b15ab91b46c
Harald Kirschner <hkirschner@microsoft.com>
2024-02-12 16:25:55 -0800

    v0

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,75 @@
 "use client";
-import Image from "next/image";
 import { useEffect, useState } from "react";
+import { Agents, advisorList, TranscriptSummary } from "../lib/llm";
+import { transcript as simulation } from "../lib/simulate2";
+import { FaPlay, FaPause, FaCog, FaMicrophone, FaRobot } from "react-icons/fa";
+
+const agents = new Agents(process.env.NEXT_PUBLIC_OPENAI_API_KEY || "");
+
+interface Transcript {
+  text: string;
+  analysis: TranscriptSummary | null;
+}
 
 export default function Home() {
-  const [transcript, setTranscript] = useState("");
-  const [isRecognitionStarted, setIsRecognitionStarted] = useState(false);
+  const [transcripts, setTranscripts] = useState<Transcript[]>([]);
+  const [TTSRunning, setTTSRunning] = useState(false);
+  const [simulationRunning, setSimulationRunning] = useState(false);
   const [interimTranscript, setInterimTranscript] = useState("");
-  const [lastFinalTranscript, setLastFinalTranscript] = useState(""); // Added state for lastFinalTranscript
+  const [advisor, setAdvisor] = useState(advisorList[0]);
+
+  const addTranscript = async (text: string) => {
+    const newTranscript: Transcript = {
+      text,
+      analysis: null,
+    };
+    setTranscripts((prev) => [...prev, newTranscript]);
+
+    agents
+      .reviewLatest(advisor, [
+        ...transcripts.map((transcript) => transcript.text),
+        text,
+      ])
+      .then((analysis) => {
+        setTranscripts((prev) => {
+          return prev.map((transcript) => {
+            if (transcript.text === newTranscript.text) {
+              return { ...transcript, analysis };
+            }
+            return transcript;
+          });
+        });
+      });
+  };
+
+  useEffect(() => {
+    const startSimulation = async () => {
+      for (const text of simulation) {
+        for (const word of text.split(/[^\w]+/)) {
+          if (!simulationRunning) {
+            break;
+          }
+          setInterimTranscript((prev) => prev + " " + word);
+          await new Promise((resolve) => setTimeout(resolve, word.length * 50));
+        }
+        if (!simulationRunning) {
+          break;
+        }
+        setInterimTranscript("");
+        addTranscript(text);
+      }
+      setSimulationRunning(false);
+    };
+
+    if (simulationRunning) {
+      startSimulation();
+    }
+  }, [simulationRunning]);
 
   useEffect(() => {
     let recognition: SpeechRecognition | undefined;
 
-    const startRecognition = () => {
+    const startTTS = () => {
       if ("SpeechRecognition" in window) {
         recognition = new window.SpeechRecognition();
       } else if ("webkitSpeechRecognition" in window) {
@@ -24,34 +82,28 @@ export default function Home() {
       recognition.interimResults = true;
       recognition.continuous = true;
 
-      recognition.onresult = (event: SpeechRecognitionEvent) => {
+      // @ts-ignore
+      recognition.onresult = async (event: SpeechRecognitionEvent) => {
         const result = event.results[event.results.length - 1];
         const text = result[0].transcript;
 
-        // console.log(result, event.results.length);
-
         if (result.isFinal) {
-          if (text == lastFinalTranscript) {
-            return;
-          }
           console.log("final transcript", text);
-          console.log("lastFinalTranscript", lastFinalTranscript, "transcript", transcript);
-          
-          setTranscript((prev) => (prev + " " + lastFinalTranscript).trim());
-          setLastFinalTranscript(text);
-          setInterimTranscript('');
+          console.log("transcripts", transcripts);
+
+          setInterimTranscript("");
+          addTranscript(text);
         } else {
           console.log("interim transcript", text);
           setInterimTranscript(text);
         }
-
       };
 
       recognition.start();
     };
 
-    if (isRecognitionStarted) {
-      startRecognition();
+    if (TTSRunning) {
+      startTTS();
     }
 
     return () => {
@@ -59,27 +111,103 @@ export default function Home() {
         recognition.stop();
       }
     };
-  }, [isRecognitionStarted]);
-
-  useEffect(() => {
-    setIsRecognitionStarted(true);
-  }, []);
+  }, [TTSRunning, transcripts]);
 
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <div className="text-center">
-        {!isRecognitionStarted && (
-          <button
-            className="rounded-lg px-5 py-4 bg-red-500 text-white text-2xl hover:bg-gray-300"
-            onClick={() => setIsRecognitionStarted(true)}
+    <>
+      <main className="flex-grow flex items-center justify-center p-1 overflow-y-auto bg-black">
+        <div>
+          <div className="flex-row-reverse">
+            {transcripts.map((transcript, index) => (
+              <div
+                key={index}
+                className={
+                  "flex m-1 rounded-lg text-gray-900 " +
+                  (transcript.analysis
+                    ? transcript.analysis?.speaker == 1
+                      ? "bg-blue-100"
+                      : "bg-green-100"
+                    : "bg-gray-100")
+                }
+              >
+                <div className="italic text-lg p-2 w-1/3">
+                  {transcript.text}
+                </div>
+                <div className="text-black text-xl bg-white leading-tight p-2 px-4 w-2/3">
+                  {transcript.analysis ? (
+                    <>
+                      <div className="text-gray-600">
+                        {transcript.analysis.insight}
+                      </div>
+                      <div className="text-red-900 font-semibold">
+                        {transcript.analysis.recommendation}
+                      </div>
+                    </>
+                  ) : (
+                    "..."
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="text-3xl text-gray-500">{interimTranscript}</div>
+          {(TTSRunning || simulationRunning) ? (
+            <div className="text-3xl text-gray-500">
+              <FaMicrophone className="animate-pulse ease-in-out duration-75" />
+            </div>
+          ) : (
+            <div className="text-5xl flex items-center text-gray-300">
+              <span className="font-bold uppercase">Ready! Set!</span>{" "}
+              <FaMicrophone className="ml-2 inline" />
+            </div>
+          )}
+        </div>
+      </main>
+      <nav className="bg-white shadow-md p-2 flex justify-around">
+        <button
+          className={`flex items-center rounded-md px-3 py-1 ${
+            !TTSRunning ? "" : "bg-red-400 animate-pulse ease-in-out	duration-75"
+          }`}
+          onClick={() => setTTSRunning(!TTSRunning)}
+        >
+          {TTSRunning ? (
+            <FaPause className="mr-1" />
+          ) : (
+            <FaMicrophone className="mr-1" />
+          )}
+          <span>{TTSRunning ? "Pause" : "Listen"}</span>
+        </button>
+        <button
+          className={`flex items-center rounded-md px-3 py-1 ${
+            !simulationRunning
+              ? ""
+              : "bg-red-400 animate-pulse ease-in-out	duration-75"
+          }`}
+          onClick={() => setSimulationRunning(!simulationRunning)}
+        >
+          {simulationRunning ? (
+            <FaPause className="mr-1" />
+          ) : (
+            <FaRobot className="mr-1" />
+          )}
+          <span>Simulate</span>
+        </button>
+        <div className="flex items-center">
+          <FaCog className="mr-1" />
+          <span>Advisor: </span>
+          <select
+            value={advisor}
+            onChange={(e) => setAdvisor(e.target.value)}
+            className="ml-1"
           >
-            Start Transcript
-          </button>
-        )}
-        <span className="text-4xl text-blue-500">{transcript}</span>
-        <span className="text-4xl text-green-500">{lastFinalTranscript}</span>
-        <span className="text-4xl text-gray-500 italic">{interimTranscript}</span>
-      </div>
-    </main>
+            {advisorList.map((advisor) => (
+              <option key={advisor} value={advisor}>
+                {advisor}
+              </option>
+            ))}
+          </select>
+        </div>
+      </nav>
+    </>
   );
 }
---
+++ lib/llm.ts
@@ -0,0 +1,142 @@
+
+export type TranscriptSummary = {
+    speaker: number;
+    insight?: string;
+    recommendation?: string;
+};
+
+export interface AdvisorDetails {
+    conversation: string;
+    participants: string[] | string;
+    feedback?: string;
+    audience?: string;
+    language?: string;
+    outcome: string;
+}
+
+const advisors: Record<string, AdvisorDetails> = {
+    "Job Interview": {
+        conversation: "job interview for a marketing position",
+        participants: ["interviewer, asking probing questions", "job candidate, trying to make a good impression"],
+        feedback: "missed to answer, unclear, too technical, too vague, too long, too short, etc.",
+        audience: "job candidate",
+        outcome: "help the job candidate improve their chances of getting hired",
+    },
+    "Spanish Practice": {
+        conversation: "conversation in Spanish",
+        participants: "multiple spanish learners",
+        feedback: "Point out SPECIFIC vocabulary, grammar, and expression mistakes. In English.",
+        audience: "spanish learners",
+        outcome: "help the learners get a better grasp of the language and improve their speaking skills",
+        language: "es-ES",
+    },
+    "Movie Script": {
+        conversation: "movie script reading",
+        participants: "various actors",
+        feedback: "script weaknesses, character development, plot holes, etc.",
+        audience: "screen writer",
+        outcome: "make a great movie script that is captivating and engaging",
+    },
+    "Product Demo": {
+        conversation: "product demonstration for potential clients",
+        participants: ["sales representative, presenting the product", "potential clients, evaluating the product"],
+        feedback: "presentation skills, product knowledge, handling objections, etc.",
+        audience: "sales representative",
+        outcome: "improve the product demonstration to increase sales",
+    },
+    "Status Meeting": {
+        conversation: "weekly status meeting",
+        participants: "team members, reporting on their progress",
+        feedback: "communication clarity, time management, task tracking, etc.",
+        audience: "team members and project manager",
+        outcome: "improve the efficiency and effectiveness of status meetings",
+    }
+};
+
+export const advisorList = Object.keys(advisors);
+
+export class Agents {
+    private apiKey: string;
+
+    constructor(apiKey: string) {
+        this.apiKey = apiKey;
+    }
+
+    private async fetchOpenaiAPI(messages: any[], maxTokens = 10) {
+        const apiUrl = "https://api.openai.com/v1/chat/completions";
+        try {
+            const response = await fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${this.apiKey}`,
+                },
+                body: JSON.stringify({
+                    model: "gpt-3.5-turbo-0125",
+                    response_format: { "type": "json_object" },
+                    max_tokens: maxTokens,
+                    messages: messages,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to fetch OpenAI API: ${response.status} ${response.statusText}`);
+            }
+
+            return response.json();
+        } catch (error) {
+            console.error("Error calling OpenAI API:", error);
+            throw error;
+        }
+    }
+
+    public async reviewLatest(advisorId: string, transcript: string[]): Promise<TranscriptSummary> {
+        const advisor = advisors[advisorId];
+        if (!advisor) {
+            throw new Error(`Advisor not found: ${advisorId}`);
+        }
+        const messages = [
+            {
+                role: "system",
+                content: `
+                    You are an expert mentor that provides real-time feedback on a transcript from a live conversation. You are provided transcript snapshot. Your goal is to analyze the transcript for actionable feedback and follow-up guidance. You ent excellent listening skills, deep understanding of human behavior, and a knack for giving actionable advice that helps in the moment.
+
+                    # Conversation Details
+                    Context: Conversation is a ${advisor.conversation}.
+                    ${Array.isArray(advisor.participants) ? advisor.participants.map((participant, idx) => `Participant ${idx + 1}: ${participant}`).join("\n") : `Participants: ${advisor.participants}`
+                    }
+                    Feedback: Will be read by ${advisor.audience || advisor.participants[0]}.
+                    
+                    # Think step-by-step:
+                    Step 1: Read the summary and latest transcript line carefully and make informed assumptions about the speakers, context, subtext, open questions, and hidden messages.
+                    Step 2: Based on the LAST LINE and prior conversation provide constructive feedback (${advisor.feedback}) with specific and actionable guidance for follow-up (10 words maximum).
+                    Step 3: Return JSON (Typescript type: { speaker: number; recommendation?: string; };). Skip optional values if not applicable. Keep the JSON compact with no added whitespace or line breaks.
+
+                    # Remember
+                    - Your feedback will be read by ${advisor.audience || advisor.participants[0]} as they are having the conversation, so it should be concise (easy to scan) and actionable (can be applied here and now).
+                    - Do not provide recommendations for later practice or general advice, but focus on specific and direct feedback that can be applied real-time in the moment.
+                    - If you find a mistake, point it out directly and provide a correction. Avoid socratic questioning or indirect feedback.
+
+                    # Your Work Matters!
+                    If you provide good real-time mentoring, you will ${advisor.outcome}, and you will earn the Mentor Master badge. Good luck!
+                `.replace(/\n\s+/g, "\n"),
+
+                // Step 2: For the LAST LINE, determine the subtext behind it.Look for clues such as tone, emphasis, irony, sarcasm, humor, or deception.Identify the hidden or implied messages that the speakers are trying to convey or conceal.If the LAST LINE has a hidden or implied meaning, summarize it as actionable insight(10 words maximum), otherwise skip this step.
+            },
+            {
+                role: "user",
+                content: `# SUMMARY\n${transcript.slice(0, transcript.length - 1).join("\n")}`,
+            },
+            {
+                role: "user",
+                content: `# LAST LINE\n${transcript[transcript.length - 1]}`
+            }
+        ];
+
+        const response = await this.fetchOpenaiAPI(messages, 500);
+        const message = JSON.parse(response.choices[0].message.content);
+        console.log(message);
+
+        return (Array.isArray(message) ? message[0] : message) as TranscriptSummary;
+    }
+}
